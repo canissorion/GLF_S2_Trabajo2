0ce96f05539052e61f9a3c5d3baf61177642432c
Zrk00 <mmoreno@utem.cl>
2021-10-22 22:09:21 -0300

    ADD: OPERACIONES

--- components/HomeComponent.vue
+++ components/HomeComponent.vue
@@ -121,7 +121,104 @@
             </div>
             
         </div>
-            
+       <h1 class="text-center fredoka textocolor my-4">Operaciones</h1>
+        <div class="row justify-content-center">
+            <div class="card cardaux4 col-md-10 rounded-top">
+                <div class="btn-group justify-content-center" role="group">
+                    <a href="#afd-equivalente"><button class="btn btn-secondary" >AFD equivalente</button></a>
+                    <a href="#complemento"><button class="btn btn-secondary" >Complemento</button></a>
+                    <a href="#union"><button class="btn btn-secondary" >Unión</button></a>
+                    <a href="#concatenacion"><button class="btn btn-secondary" >Concatenación</button></a>
+                    <a href="#interseccion"><button class="btn btn-secondary" >Intersección</button></a>
+                </div>
+            </div>
+
+            <!--Caso 1: AFD EQUIVALENTE -->
+            <div class="cardaux2 col-md-10" >
+                <h3 class="text-center fredoka textocolor my-3">AFD equivalente</h3>
+            </div>
+            <div id="afd-equivalente" class="card cardaux3 col-md-10 rounded-bottom mb-3" >
+                <div class="container my-3">
+                    La conversión implica pasar por un AFD intermedio con estados y transiciones redundantes, luego se eliminan los estados inaccesibles o inalcanzables, es decir, aquellos a los que no se puede acceder a partir del estado inicial. Luego de este procedimiento, se obtiene el AFD equivalente.
+                     <div class="my-3 text-center">
+                        <button class="btn btn-success" >Mostrar AFD Equivalente</button>
+                    </div>      
+                    <div id="equivalente" class="mb-3" style="border: 1px solid lightgray;"></div>
+                </div>
+            </div>
+
+            <!--Caso 2: COMPLEMENTO -->
+            <div class="cardaux2 col-md-10" >
+                <h3 class="text-center fredoka textocolor my-3">Complemento</h3>
+            </div>
+            <div id="afd-equivalente" class="card cardaux3 col-md-10 rounded-bottom mb-3" >
+                <div class="container my-3">
+                    Esta propiedad es solo para AFD, intercambia los estados finales por no finales.
+                    <div class="my-3 text-center">
+                        <button class="btn btn-success" >Mostrar Complemento Autómata 1</button>
+                        <button class="btn btn-success" >Mostrar Complemento Autómata 2</button>
+                    </div> 
+                    <h4 class="text-center fredoka textocolor my-3" >Complemento Autómata 1</h4>   
+                    <h4 class="text-center fredoka textocolor my-3" >Complemento Autómata 2</h4>
+                    <div id="complemento" class="mb-3" style="border: 1px solid lightgray;"></div>
+                    <div class="my-3 text-center" >
+                        <h4 class="text-center fredoka textocolor my-3">AFD equivalente simplificado</h4>
+                        <button class="btn btn-success" >Mostrar Autómata</button>
+                        <div id="afdSimplificado" class="mb-3" style="border: 1px solid lightgray;"></div>
+                    </div>
+
+                </div>
+            </div>
+
+            <!--Caso 3: UNION -->
+            <div class="cardaux2 col-md-10"  >
+                <h3 class="text-center fredoka textocolor my-3">Unión</h3>
+            </div>
+            <div id="afd-equivalente" class="card cardaux3 col-md-10 rounded-bottom mb-3" >
+                <div class="container my-3">
+                    La unión de dos lenguajes regulares es otro lenguaje regular, donde se utiliza la operación de unión de conjuntos, de manera que, Σ = {x,y} si L1 = {x,xy} y L2 = {yx,yy} la unión es L1UL2 = {x,xy,yx,yy}.
+                    <div class="my-3 text-center">
+                        <button class="btn btn-success" >Mostrar unión</button>
+                    </div>      
+                    <div id="union" class="mb-3" style="border: 1px solid lightgray;"></div>
+                </div>
+            </div>
+
+            <!--Caso 4: CONCATENACION -->
+            <div class="cardaux2 col-md-10" >
+                <h3 class="text-center fredoka textocolor my-3">Concatenación</h3>
+            </div>
+            <div id="afd-equivalente" class="card cardaux3 col-md-10 rounded-bottom mb-3" >
+                <div class="container my-3">
+                    Se concatena una una cadena del primer lenguaje y una cadena del segundo lenguaje.
+                    <div class="my-3 text-center">
+                        <button class="btn btn-success" >Mostrar concatenación</button>
+                    </div>      
+                    <div id="concatenacion" class="mb-3" style="border: 1px solid lightgray;"></div>
+                </div>
+            </div>
+
+            <!--Caso 5: INTERSECCION -->
+            <div class="cardaux2 col-md-10" >
+                <h3 class="text-center fredoka textocolor my-3">Intersección</h3>
+            </div>
+            <div id="afd-equivalente" class="card cardaux3 col-md-10 rounded-bottom mb-3" >
+                <div class="container my-3">
+                    La intersección de ambos lenguajes utiliza la operación de interseccion de conjuntos, Automata1 ∩ Automata2: 
+                    <div class="my-3 text-center">
+                        <button class="btn btn-success" >Mostrar Intersección</button>
+                    </div>      
+                    <div id="interseccion" class="mb-3" style="border: 1px solid lightgray;"></div>
+                </div>
+                <div class="text-center">
+                    <button class="btn btn-success" >SIMPLIFICA 1</button>
+                    <div id="afdSimplificado" class="mb-3" style="border: 1px solid lightgray;"></div>
+                </div>
+                <div class="text-center">
+                    <button class="btn btn-success" >SIMPLIFICA 2</button>
+                </div>
+            </div>
+        </div>     
 
     </div>
 </template>
